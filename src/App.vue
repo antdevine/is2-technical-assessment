33059f5507e29b1c7c6a670162a5ba337c71d9da
Anthony Devine <antdevine@hotmail.co.uk>
2025-06-07 17:28:37 +0100

    Removal of unnecissary code
    * Removed code installed by vue
    * Added basic frame of the form into the main app page

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,51 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue'
+
+const form = reactive({
+  name: '',
+  items: [''],
+  insureItems: true,
+  startDate: '',
+  limitOfIndemnity: '',
+  excess: ''
+})
+
+const completePolicy = () => {
+  console.log(form);
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <form @submit.prevent="completePolicy()">
+    <input v-model="form.name" placeholder="Name" />
+
+    <button @click.prevent="form.insureItems = true">Yes</button>
+    <button @click.prevent="form.insureItems = false">No</button>
+
+    <div v-if="form.insureItems">
+      <div v-for="(item, index) in form.items" :key="index">
+        <input v-model="form.items[index]" :placeholder="`Item ${index + 1}`" />
+        <button @click.prevent="form.items.splice(index, 1)">Remove</button>
+      </div>
+      <button @click.prevent="form.items.push('')">Add another</button>
+    </div>
+
+    <input type="date" v-model="form.startDate" />
+
+    <select v-model="form.limitOfIndemnity">
+      <option disabled value="">Please select</option>
+      <option value="1000">£1000</option>
+      <option value="5000">£5000</option>
+      <option value="10000">£10000</option>
+    </select>
+
+
+    <label><input type="radio" v-model="form.excess" value="250" /> £250</label>
+    <label><input type="radio" v-model="form.excess" value="500" /> £500</label>
+    <label><input type="radio" v-model="form.excess" value="1000" /> £1000</label>
+
+    <button type="submit">Submit</button>
+    </form>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
